59d712b0be0bbd319d386a0e4d6e6f5665bcf9eb
vgtomahawk <vgtomahawk@gmail.com>
2014-09-15 21:13:50 +0530

    new file:   src/ConfusionSet.java

---
+++ SpellCheck/src/ConfusionSet.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class ConfusionSet 
+{
+	ArrayList<String> candidates = new ArrayList<String>();
+	ArrayList<String> features = new ArrayList<String>();
+
+}
